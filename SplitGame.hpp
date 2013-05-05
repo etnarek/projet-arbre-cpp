0d4c1d8c05006e373baf7bacefd9ba3c7b1b2abe
etnarek <romain-fontaine@outlook.com>
2013-05-05 15:32:43 +0200

    Changing arguments and include for the SplitGame class

--- SplitGame.hpp
+++ SplitGame.hpp
@@ -2,13 +2,14 @@
 #define DEF_SPLITGAME
 
 #include <iostream>
+#include <string>
 #include "Tree.hpp"
 
 class SplitGame
 {
 public:
     // Constructeur
-    SplitGame(Tree &startTree, Tree const &endTree, int maxTry);
+    SplitGame(Tree &newStartTree, Tree const &newEndTree, int newMaxTry);
 
     bool isIdentic();
     void trig();
