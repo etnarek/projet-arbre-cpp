ebf6671d11ca96cdf1743fe11743b9b1f78ca763
etnarek <romain-fontaine@outlook.com>
2013-05-05 15:33:24 +0200

    Added function in the cpp file and create the constructeur

---
+++ SplitGame.cpp
@@ -0,0 +1,20 @@
+#include "SplitGame.hpp"
+
+// Constructeur
+SplitGame::SplitGame(Tree &newStartTree, Tree const &newEndTree, int newMaxTry){
+    startTree = &newStartTree;
+    endTree = & newEndTree;
+    maxTry = newMaxTry;
+}
+
+bool SplitGame::isIdentic(){
+
+}
+void SplitGame::trig(){
+
+}
+
+// Destructeur
+SplitGame::~SplitGame(){
+
+}
