13f5d194a4b8707ebddccb419f802992f5a7eb19
FONTAINE  Romain <rfontain@mint>
2013-05-06 10:53:08 +0200

    changing trig and sub functions in privates functions.

--- SplitGame.hpp
+++ SplitGame.hpp
@@ -14,15 +14,16 @@ public:
     // Game
     bool isIdentic();
     void trig();
-    static int trigRecurse(Tree *tree);
-    static void delation(Tree *tree, bool left);
-    static void extend(Tree *tree, bool left);
-    static void split(Tree *tree, bool left);
 
 private:
     int maxTry;
     Tree *startTree;
     const Tree *endTree;
+    
+    static int trigRecurse(Tree *tree);
+    static void delation(Tree *tree, bool left);
+    static void extend(Tree *tree, bool left);
+    static void split(Tree *tree, bool left);
 
 };
 
